322cbd26f040ead07602a146cad312a6418d8b09
Jax <93849152+StarFallenJax@users.noreply.github.com>
2024-03-06 20:57:46 -0800

    Add files via upload

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,29 @@
+package com.example;
+import com.example.items.LifestealEnchantment;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main implements ModInitializer {
+	public static Enchantment LIFESTEAL = new LifestealEnchantment();
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("enchantment");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registries.ENCHANTMENT, new Identifier("enchantment", "lifesteal"), LIFESTEAL);
+
+	}
+}
